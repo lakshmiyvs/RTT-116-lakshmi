49b1ac906a0c170ed8612b61b24f96a907af045d
lakshmiyvs <yvs.lakshmi@gmail.com>
2024-12-10 15:33:30 -0600

    Classwork 307 and 309 springboot

---
+++ Module-309/module-309/src/main/java/com/example/module_309/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.module_309.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        ModelAndView response = new ModelAndView();
+
+        // this is our index.jsp
+        response.setViewName("index");
+
+        return response;
+
+    }
+}
